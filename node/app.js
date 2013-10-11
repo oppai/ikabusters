41e6d2aebb7a00cf7a4da2022bf677a5ce472e46
kodam <hotsoup.h@gmail.com>
2013-10-11 17:33:04 +0900

    update

--- node/app.js
+++ node/app.js
@@ -20,20 +20,37 @@ var ika = {
   hp:1000,
   max_hp:1000,
 };
+var clients = new Object();
+var current_id = 0;
+
 io.sockets.on('connection', function (socket) {
+  var c = {id:current_id,count:100};
+  clients[current_id] = c;
+  current_id++;
+
   socket.on('init',function (data){
     ika = data.ika;
   });
+  
   socket.on('attacking',function (data){
     ika.hp -= data.damage;
     socket.emit('attacking',data);
     update();
   });
+
+  socket.on('alive',function (){
+    c.count = 100;
+  });
+ 
   function update(){
-    socket.emit('update',{ika:ika});
-  }
+    socket.emit('update',{ika:ika,clients:clients});
+  };
+
   setInterval(function(){
     update();
+    c.count -= 1;
+    if(c.count < 0){
+      delete clients[c.id];
+    };
   },80);
 });
-
